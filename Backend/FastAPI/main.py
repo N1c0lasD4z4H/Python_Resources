9b0241e600c933073fa3fa7f9375a0ac477154fd
N1c0lasD4z4H <ndaza@adgasd32.int>
2025-01-30 16:19:42 -0500

    Autorización OAuth2 backend mouredev

--- Backend/FastAPI/main.py
+++ Backend/FastAPI/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import products, users
+from routers import products, users, basic_auth_users
 from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
@@ -8,10 +8,11 @@ app = FastAPI()
 app.include_router(products.router)
 app.include_router(users.router)
 app.mount("/static",StaticFiles(directory="static"), name="static")
+app.include_router(basic_auth_users.router)
 
 @app.get("/")
 async def root():
-    return {"message": "HEllo FastApiS"}
+    return {"message": "HEllo FastApieS"}
 @app.get("/url")
 async def root():
     return {"url_curso": "httpd://mouredev.com/python "}
